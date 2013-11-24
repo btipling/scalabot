5701e9f4da59449d83416dd31a881a3bcd1dc5a2
Bjorn Tipling <bjorn@ambientchill.com>
2013-11-23 23:37:23 -0800

    Pull out json testing code.

--- src/main/scala/scalabot.scala
+++ src/main/scala/scalabot.scala
@@ -1,31 +1,12 @@
 package scalabot
 
-import scalaz._, Scalaz._
-import argonaut._, Argonaut._
 import scalabot.config.Config
+import scalabot.pretty.Pretty
 
 object Scalabot extends App {
-  println("Hello World")
-
-  val requestJson  =
-    """
-    {
-      "userid": "1"
-    }
-    """.stripMargin
-
-  val updatedJson: Option[Json] = for {
-    parsed <- requestJson.parseOption
-  } yield ("name", jString("testuser")) ->: parsed
-
-  val obj = updatedJson.get.obj
-  printf("Updated user: %s\n", updatedJson.toString())
-  printf("obj : %s\n", obj.toString())
-  printf("userid: %s\n", obj.get.toMap("userid"))
-  println("next getting config")
-  val res = Config.fetch
-  printf("config result: %s\n", res)
-  println("Finished")
+  val config = Config.fetch
+  Pretty.green(s"Got config: $config")
+  Pretty.red("Finished")
 }
 
 
