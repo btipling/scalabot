0e5e966c2c7aa0db9227df4ae505400e6aacc1aa
Bjorn Tipling <bjorn@ambientchill.com>
2013-11-27 09:03:11 -0800

    More working progress on irc connection.

--- src/main/scala/irc_connection.scala
+++ src/main/scala/irc_connection.scala
@@ -26,20 +26,20 @@ class Client(remote: InetSocketAddress, listener: ActorRef) extends Actor {
   manager ! Connect(remote)
 
   def receive = {
-    case CommandFailed(_: Connect) ⇒
+    case CommandFailed(_: Connect) =>
       listener ! "failed"
       context stop self
 
-    case c @ Connected(remote, local) ⇒
+    case c @ Connected(remote, local) =>
       listener ! c
       val connection = sender
       connection ! Register(self)
       context become {
-        case data: ByteString        ⇒ connection ! Write(data)
-        case CommandFailed(w: Write) ⇒ // O/S buffer was full
-        case Received(data)          ⇒ listener ! data
-        case "close"                 ⇒ connection ! Close
-        case _: ConnectionClosed     ⇒ context stop self
+        case data: ByteString => connection ! Write(data)
+        case CommandFailed(w: Write) => // O/S buffer was full
+        case Received(data) => listener ! data
+        case "close" => connection ! Close
+        case _: ConnectionClosed => context stop self
       }
   }
 }
--- src/main/scala/scalabot.scala
+++ src/main/scala/scalabot.scala
@@ -2,11 +2,22 @@ package scalabot
 
 import scalabot.config.Config
 import scalabot.pretty.Pretty
+import scalabot.irc.connection
+import scalabot.irc.listener
+import java.net.InetSocketAddress
+import akka.actor.ActorSystem
+import akka.actor.Props
 
 object Scalabot extends App {
   val config = Config.fetch
   Pretty.green(s"Got config: $config")
   Pretty.red("Finished")
+  printf("connection.Client %s\n", connection.Client)
+  val address = new InetSocketAddress("chat.freenode.net", 6667)
+  Pretty.blue(s"Connecting to $address")
+  val system = ActorSystem("IRC")
+  val irclistener = system.actorOf(Props[listener.IRCListener], name = "irclistener")
+  Pretty.blue(s"listener is $irclistener")
+  system.shutdown
 }
-
 
