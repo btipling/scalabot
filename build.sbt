76ea9d3c080c335b5c2345c50b0b0282643b5b6d
Bjorn Tipling <bjorn@ambientchill.com>
2013-11-27 08:11:01 -0800

    Progress on tcp connection.

--- build.sbt
+++ build.sbt
@@ -7,7 +7,3 @@ scalaVersion := "2.10.3"
 resolvers += "SonaType" at "https://oss.sonatype.org/content/groups/public"
 
 libraryDependencies += "io.argonaut" %% "argonaut" % "6.0.1"
-
-resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
-
-libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.3-SNAPSHOT"
---
+++ src/main/scala/irc_connection.scala
@@ -0,0 +1,46 @@
+package scalabot
+
+package irc
+
+package connection
+
+import akka.actor.{ Actor, ActorRef, Props }
+import akka.io.{ IO, Tcp }
+import akka.util.ByteString
+import java.net.InetSocketAddress
+
+
+object Client {
+  def props(remote: InetSocketAddress, replies: ActorRef) =
+    Props(classOf[Client], remote, replies)
+}
+
+class Client(remote: InetSocketAddress, listener: ActorRef) extends Actor {
+
+  import Tcp._
+  implicit val system = context.system
+
+
+  val manager = IO(Tcp)
+
+  manager ! Connect(remote)
+
+  def receive = {
+    case CommandFailed(_: Connect) ⇒
+      listener ! "failed"
+      context stop self
+
+    case c @ Connected(remote, local) ⇒
+      listener ! c
+      val connection = sender
+      connection ! Register(self)
+      context become {
+        case data: ByteString        ⇒ connection ! Write(data)
+        case CommandFailed(w: Write) ⇒ // O/S buffer was full
+        case Received(data)          ⇒ listener ! data
+        case "close"                 ⇒ connection ! Close
+        case _: ConnectionClosed     ⇒ context stop self
+      }
+  }
+}
+
